2017ede2be8262d4270859a0d803e26df12e74ed
seducerooo <>
2023-08-16 15:57:44 +0330

    pruning models done successfully with product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\Builder;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory,Prunable;
     public function scopeComparison($query){
         return $query->where('store','>','100')->orWhere('store','<','300');
+    }
+
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subMonths(5));
+    }
+
+    protected function pruning(): void
+    {
+        // ...
+        echo 'Pruning' . $this->updated_at . PHP_EOL;
     }
 }
